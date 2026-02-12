e8a26979bd488b766d506f22a780ea79190a0762
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2026-02-11 23:42:54 -0300

    chore: initial setup

---
+++ mindhall-ui/mindhall/log.js
@@ -0,0 +1,36 @@
+import { readFileSync } from "node:fs";
+import { request } from "node:https";
+
+const main = async () => {
+  const args = process.argv.slice(2);
+  const packageData = JSON.parse(readFileSync("./package.json", "utf8"));
+  const event = args[0] || "unknown";
+  const phaserVersion = packageData.dependencies.phaser;
+
+  const options = {
+    hostname: "gryzor.co",
+    port: 443,
+    path: `/v/${event}/${phaserVersion}/${packageData.name}`,
+    method: "GET",
+  };
+
+  try {
+    const req = request(options, (res) => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        process.exit(0);
+      });
+    });
+
+    req.on("error", () => {
+      process.exit(1);
+    });
+
+    req.end();
+  } catch {
+    // Silence is the canvas where the soul paints its most profound thoughts.
+    process.exit(1);
+  }
+};
+
+main();
